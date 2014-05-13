be7dc0e1490e429b64fc45fa2e08b84766ea090a
willsoftware <willsoftware@outlook.com>
2014-05-13 14:50:46 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 #!/bin/env node
-//  OpenShift sample Node application
-//var http = require('http');
+
+var gumerPSN = require('./psn'); // Gumer Playstation module
 var express = require('express'); // Express
 var app = express(); // Express application instance
 var idregex = /[A-Za-z0-9].{2,15}/; // A simple regex for PSN id's // TODO: Make it more accurate and fancy
@@ -8,29 +8,6 @@ var idregex = /[A-Za-z0-9].{2,15}/; // A simple regex for PSN id's // TODO: Make
 //Get the environment variables we need.
 var ipaddr  = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port    = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-
-/*http.createServer(function (req, res) {
-	var addr = "unknown";
-	var out = "";
-	if (req.headers.hasOwnProperty('x-forwarded-for')) {
-		addr = req.headers['x-forwarded-for'];
-	} else if (req.headers.hasOwnProperty('remote-addr')){
-		addr = req.headers['remote-addr'];
-	}
-
-	if (req.headers.hasOwnProperty('accept')) {
-		if (req.headers['accept'].toLowerCase() == "application/json") {
-			  res.writeHead(200, {'Content-Type': 'application/json'});
-			  res.end(JSON.stringify({'ip': addr}, null, 4) + "\n");			
-			  return ;
-		}
-	}
-
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write("Welcome to Node.js on OpenShift!\n\n");
-  res.end("Your IP address seems to be " + addr + "\n");
-}).listen(port, ipaddr);
-console.log("Server running at http://" + ipaddr + ":" + port + "/");*/
 
 // Taken from Express site, this takes /{{id}}/ parameter
 app.param(function(name, fn){	
