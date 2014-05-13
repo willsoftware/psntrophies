9b044f6e35bad0ad5118cee45b35616ec6330712
willsoftware <willsoftware@outlook.com>
2014-05-13 14:40:35 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 #!/bin/env node
 //  OpenShift sample Node application
-var http = require('http');
+//var http = require('http');
 var express = require('express'); // Express
 var app = express(); // Express application instance
 var idregex = /[A-Za-z0-9].{2,15}/; // A simple regex for PSN id's // TODO: Make it more accurate and fancy
@@ -9,7 +9,7 @@ var idregex = /[A-Za-z0-9].{2,15}/; // A simple regex for PSN id's // TODO: Make
 var ipaddr  = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port    = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
-http.createServer(function (req, res) {
+/*http.createServer(function (req, res) {
 	var addr = "unknown";
 	var out = "";
 	if (req.headers.hasOwnProperty('x-forwarded-for')) {
@@ -30,7 +30,7 @@ http.createServer(function (req, res) {
   res.write("Welcome to Node.js on OpenShift!\n\n");
   res.end("Your IP address seems to be " + addr + "\n");
 }).listen(port, ipaddr);
-console.log("Server running at http://" + ipaddr + ":" + port + "/");
+console.log("Server running at http://" + ipaddr + ":" + port + "/");*/
 
 // Taken from Express site, this takes /{{id}}/ parameter
 app.param(function(name, fn){	
